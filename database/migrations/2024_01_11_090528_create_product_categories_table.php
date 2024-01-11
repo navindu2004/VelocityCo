dcf6c440805bbd4e9a0d41d7ebb039e4c8671e8d
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2024-01-11 14:50:40 +0530

    done in class 11th January

---
+++ database/migrations/2024_01_11_090528_create_product_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->boolean('status')->default(true);
+            $table->string('image')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->longText('meta_description')->nullable();
+            $table->longText('meta_keywords')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
