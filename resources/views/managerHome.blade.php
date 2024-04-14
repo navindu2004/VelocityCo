891cc4bf9deeb66208251d28742d0581782b5cd9
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2024-04-14 23:39:41 +0530

    managerhome updated

--- resources/views/managerHome.blade.php
+++ resources/views/managerHome.blade.php
@@ -8,7 +8,165 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    You are an Manager User.
+                <html>
+
+<head>
+    <title>Turtle Community in Sri Lanka</title>
+    <link rel="stylesheet" href="TStyles.css">
+</head>
+
+<body>
+
+    <div class="hero-image1">
+        <div class="hero-text1">
+          <h1 style="font-size:50px">TURTLE POPULATION</h1>
+          <p>Sri Lanka</p>
+        
+        </div>
+        <img src="D:\360_F_565733952_Q43U5mBQZvC2DDWVUJjGcTSFKCMxPKV2.jpg" class="logo">
+        <ul>
+            <li><a href="index.html"><b>Home</b></a></li>
+            <li><a href="Categories.HTML"><b>Categories</b></a></li>
+            <li><a href="Hikkaduwa Turtle Hatchery.html"><b>Hikkaduwa Turtle Hatchery</b></a></li>
+            <li><a href="Threats.html"><b>Threats</b></a></li>
+            <li>
+            <div class="dropdown">
+                <button class="dropbtn"><b>Other</b> 
+                  <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                  <a href="Kosgoda Turtle Hatchery.html">Kosgoda</a>
+                  <a href="Bentota Turtle Hatchery.html">Bentota</a>
+                  
+                </div>
+              </div>
+            </li>
+            <li><a href="Donations.html"><b>Donations</b></a></li>
+         
+        </ul>
+        </div>
+        </div>
+    <br>
+    <br>
+
+    <p>Turtles also known as Testudines are reptiles that has a special shell developed mainly from their ribs.Turtles
+        are divided into two main groupd as Pleurodira(side necked turtles) and Cryptodira (hidden necked turtles)
+        ,which differ according to the way their head retracts.There are about 360 living and recently extinct species
+        of turtles.Turtles are normally rarely seen because sea is their home and only the female turtles come ashore
+        and that too when its dark.The land turtles are seen very frequently.As for today there are 250 species of
+        turtles on Earth.7 out of those live in the sea so they are called sea turtles.The other 243 species live in the
+        land or in freshwater ponds and marshes.The terestrial turtles who lives solely on land are called
+        Tortoises.Five species of sea turtles in the world visit Sri Lanka to breed.</p>
+    <p>Turtles have appeared in myths and folktales around the world. Some terrestrial and freshwater species are widely
+        kept as pets. Turtles have been hunted for their meat, for use in traditional medicine, and for their shells.
+        Sea turtles are often killed accidentally as bycatch in fishing nets. Turtle habitats around the world are being
+        destroyed. As a result of these pressures, many species are extinct or threatened with extinction.</p>
+    <ol type="1" class="ol1">
+        <li>Features
+            <ul type="disc" class="ul1">
+                <li>Size</li>
+                <li>Shell</li>
+                <li>Head</li>
+                <li>Limbs</li>
+            </ul>
+        </li>
+        <li>Lifespan</li>
+        <li>Food</li>
+        <li>Habitat</li>
+
+    </ol>
+    <hr>
+    <h2>FEATURES</h2>
+    <p>
+        <img src="D:\WDOS-ASSIGNMENT\ImageS\desktop-wallpaper-baby-sea-turtle-iphone-in-2020-sea-turtle-turtle-baby-sea-turtle-cute-turtle-thumbnail.jpg"
+            width="200" height="340" class="img1">
+
+        Turtle shells are made mostly of bone; the upper part is the domed carapace, while the underside is the flatter
+        plastron or belly-plate. Its outer surface is covered in scales made of keratin, the material of hair, horns,
+        and claws. The carapace bones develop from ribs that grow sideways and develop into broad flat plates that join
+        up to cover the body. Turtles are ectotherms or "cold-blooded", meaning that their internal temperature varies
+        with their direct environment. They are generally opportunistic omnivores and mainly feed on plants and animals
+        with limited movements. Many turtles migrate short distances seasonally. Sea turtles are the only reptiles that
+        migrate long distances to lay their eggs on a favored beach.
+    </p>
+    <br>
+    <hr>
+    <h2>LIFESPAN</h2>
+    <p>Turtles can live long lives. The oldest living turtle and land animal is said to be a Seychelles giant tortoise
+        named Jonathan, who turned 187 in 2019. A Galápagos tortoise named Harriet was collected by Charles Darwin in
+        1835; it died in 2006, having lived for at least 176 years. Most wild turtles do not reach that age. Turtles
+        keep growing new scutes under the previous scutes every year, allowing researchers to estimate how long they
+        have lived. They also age slowly. The survival rate for adult turtles can reach 99% per year.</p>
+        <br>
+    <hr>
+    <h2>FOOD</h2>
+    <p>Most turtle species are opportunistic omnivores; land-dwelling species are more herbivorous and aquatic ones more
+        carnivorous.Generally lacking speed and agility, most turtles feed either on plant material or on animals with
+        limited movements like mollusks, worms, and insect larvae.Some species, such as the African helmeted turtle and
+        snapping turtles, eat fish, amphibians, reptiles (including other turtles), birds, and mammals. They may take
+        them by ambush but also scavenge. The alligator snapping turtle has a worm-like appendage on its tongue that it
+        uses to lure fish into its mouth. Tortoises are the most herbivorous group, consuming grasses, leaves, and
+        fruits.Many turtle species, including tortoises, supplement their diet with eggshells, animal bones, hair, and
+        droppings for extra nutrients.</p>
+        <br>
+    <img src="D:\WDOS-ASSIGNMENT\ImageS\icegif-923.gif" width="500px" class="gif1">
+    <hr>
+    <h2>HABITAT</h2>
+    <p>Turtles have adapted to a remarkable variety of environments, but the greatest number of species occur in
+        southeastern North America and South Asia. In both areas, most species are aquatic, living in bodies of water
+        ranging from small ponds and bogs to large lakes and rivers. A few are strictly terrestrial (tortoises), and
+        others divide their time between land and water.</p>
+        <br>
+        <br>
+        <img src="ImageS/yXrOr7.gif" width="500px" class="gif2">
+        
+        <br>
+
+
+
+
+
+
+
+
+
+
+        <footer>
+            <div class="row primary">
+                <div class="column about">
+                    <h3>Help Us Save The Turtles</h3>
+                    <p>
+                        Turtles plays a vital role in maintaining the health of the World's oceans for more than 100 million years.So it's our responsibility to protect these animals which are a gift from nature.
+                    </p>
+                </div>
+                <div class="column links">
+                    <h3>Quick SneakPeak</h3>
+                    <div class="ul90">
+                    <ul>
+                        <li>
+                            <a href="DonationsTurtles.html">Donate Now</a>
+                        </li>
+                        <li>
+                            <a href="Merch.html">Purchase Merch</a>
+                        </li>
+                        <li>
+                            <a href="Categories.HTML">Learn More</a>
+                        </li>
+    
+                    </ul>
+                    </div>
+                </div>
+    
+    
+    
+    
+            </div>
+    </footer>
+
+
+</body>
+
+</html>
 
                 </div>
             </div>
