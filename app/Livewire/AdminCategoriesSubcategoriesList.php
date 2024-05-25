1dade4b327c37ab1d81fefcd2752421c0e813f15
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2024-05-25 23:46:26 +0530

    feat: Add AdminCategoriesSubcategoriesList component and view
    This commit adds the `AdminCategoriesSubcategoriesList` component and the `admin-categories-subcategories-list.blade.php` view file. The `AdminCategoriesSubcategoriesList` component is responsible for rendering the list of categories and subcategories in the admin panel. This addition improves the organization and management of categories and subcategories in the application.

---
+++ app/Livewire/AdminCategoriesSubcategoriesList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AdminCategoriesSubcategoriesList extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-categories-subcategories-list');
+    }
+}
