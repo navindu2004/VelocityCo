6374df4b68cc3831787f501f2bf150399f173ac3
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2024-05-28 23:58:47 +0530

    feat: Add seller verification email template
    This commit adds a new email template for seller verification. The template includes a personalized message with the seller's name and email, along with a verification link. This enhancement improves the user experience by providing clear instructions for verifying a seller account.

---
+++ resources/views/email-templates/seller-verify-template.blade.php
@@ -0,0 +1,5 @@
+<p>Dear {{ $seller_name }}</p>
+<p>
+    We have received your request to create a seller account associated with {{ $seller_email }}.<br> Please click on the link below to verify your account:<br>
+    <a href="{{ $action_link }}">Verify Account</a>
+</p>
